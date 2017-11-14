b3aafec82d4bca7c593827c751b2d080128a5da1
fufjvnvnf <fufjvnvnf@gmail.com>
2017-11-13 23:26:13 -0500

    added json parser in core

--- core.mli
+++ core.mli
@@ -1,3 +1,5 @@
+open Yojson.Basic
+
 (* [diff] represents an ocaml diff object *)
 type diff
 
@@ -8,3 +10,10 @@ val calc_diff : string list -> string list -> diff
 (* [update_diff base_content diff_content] is the result that we apply
  * [diff_content] to the [base_content]  *)
 val update_diff : string list -> diff -> string list
+
+(* [parse_json diff_json] returns an ocaml diff object
+ * represented by the diff json *)
+val parse_json : json -> diff
+
+(* [build_json diff_obj] returns the diff json of the ocaml diff object *)
+val build_json : diff -> json

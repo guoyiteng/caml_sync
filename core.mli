d7b82ae9580d04155adf3575c1d5aeb1520f5c55
Yiteng Guo <yg229@cornell.edu>
2017-11-13 22:16:13 -0500

    [spec] add spec for core.mli

--- core.mli
+++ core.mli
@@ -1,5 +1,9 @@
 type diff
 
+(* [calc_diff base_content new_content] is the diff result between
+ * [base_content] and the [new_content] *)
 val calc_diff : string list -> string list -> diff
 
+(* [update_diff base_content diff_content] is the result that we apply
+ * [diff_content] to the [base_content]  *)
 val update_diff : string list -> diff -> string list

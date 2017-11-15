d08cc5bc946329c2fa6467ff3e75f5051836e7ee
Yiteng Guo <yg229@cornell.edu>
2017-11-14 22:22:54 -0500

    [add] add server spec

--- core.mli
+++ core.mli
@@ -1,7 +1,8 @@
-open Yojson.Basic
-
 (* [diff] represents an ocaml diff object *)
 type diff
+
+exception File_existed
+exception File_not_found
 
 (* [calc_diff base_content new_content] is the diff result between
  * [base_content] and the [new_content] *)
@@ -13,7 +14,17 @@ val update_diff : string list -> diff -> string list
 
 (* [parse_json diff_json] returns an ocaml diff object
  * represented by the diff json *)
-val parse_json : json -> diff
+val parse_json : [> Ezjsonm.t ] -> diff
 
 (* [build_json diff_obj] returns the diff json of the ocaml diff object *)
-val build_json : diff -> json
+val build_json : diff -> [> Ezjsonm.t ]
+
+(* [create_file filename content] creates a new file named [filename]. [filename]
+ * should include any directory if wanted. [content] is a list of lines
+ * representing the content.
+ * raise: [File_existed] if there has already existed a file at [filename]. *)
+val create_file : string -> string list -> unit
+
+(* [delete_file filename]
+ * raise: [File_not_found] *)
+val delete_file : string -> unit
--- server.mli
+++ server.mli
@@ -1,1 +1,26 @@
 open Core
+open Opium.Std
+
+type version = int
+
+val cur_version: int
+
+(* Handle GET request at "/version".
+ * returns: a json containing [cur_version] to the client. *)
+val handle_get_current_version: App.builder
+
+(* Handle POST request at "/diff". 
+ * accepts: the diff json between the current client version and the latest
+ * server version
+ * effects: creates a new version in the server to store this new diff and update
+ * [cur_version] to [cur_version + 1].
+ * returns: a json containing the [cur_version] to the client. *)
+val handle_post_diff_from_client: App.builder
+
+(* Handle GET request at "/diff/:client_version"
+ * accepts: [client_version] is the current client version number.
+ * returns: a diff json containing the different between the client version and
+ * current server version. [cur_version] should also be included and sent back
+ * to the client. *)
+val handle_get_diff_from_client: App.builder
+

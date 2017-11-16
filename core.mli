681f7103b57f066d4df244783d30d1d2aa97d998
SereneWarrior <yy372@cornell.edu>
2017-11-15 20:45:45 -0500

    updated comments in core

--- core.mli
+++ core.mli
@@ -1,4 +1,4 @@
-(* [diff] represents an ocaml diff object between contents*)
+(* [diff] represents an ocaml diff object between contents *)
 type diff
 
 
@@ -17,30 +17,31 @@ type version_diff = {
 exception File_existed
 exception File_not_found
 
-(* [calc_diff base_content new_content] is the diff result between
- * [base_content] and the [new_content] *)
+(* [calc_diff base_content new_content] returns the difference between
+ * [base_content] and [new_content] *)
 val calc_diff : string list -> string list -> diff
 
-(* [update_diff base_content diff_content] is the result that we apply
- * [diff_content] to the [base_czontent]  *)
+(* [update_diff base_content diff_content] returns the result after applying the changes in
+ * [diff_content] to [base_content] *)
 val update_diff : string list -> diff -> string list
 
 (* [parse_json diff_json] returns an ocaml diff object
  * represented by the diff json *)
 val parse_json : [> Ezjsonm.t ] -> diff
 
-(* [build_json diff_obj] returns the diff json of the ocaml diff object*)
+(* [build_json diff_obj] returns the diff json containing all the information 
+ * in the ocaml diff object [diff] *)
 val build_json : diff -> [> Ezjsonm.t ]
 
-(* [write_json json filename] writes the json to output file. *)
+(* [write_json json filename] writes the json to an output file specified by [filename] *)
 val write_json : [> Ezjsonm.t ] -> string -> unit
 
 (* [create_file filename content] creates a new file named [filename]. [filename]
  * should include any directory if wanted. [content] is a list of lines
- * representing the content.
- * raise: [File_existed] if there has already existed a file at [filename]. *)
+ * representing the content to be written to the new file.
+ * raises: [File_existed] if there already exists a file with the same name as [filename] *)
 val create_file : string -> string list -> unit
 
-(* [delete_file filename] deletes a file named [file].
- * raise: [File_not_found] *)
+(* [delete_file filename] deletes a file named [filename].
+ * raises: [File_not_found] if [filename] cannot be found *)
 val delete_file : string -> unit

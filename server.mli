3eeb1054d05bf76eaef73adad0cb60fbd3d18ac9
SereneWarrior <yy372@cornell.edu>
2017-11-15 21:02:29 -0500

    updated server.mli

--- server.mli
+++ server.mli
@@ -13,21 +13,21 @@ type config = {
 }
 
 (* [init token] creates a caml_sync server directory structure in the current
- * directory if it does not exist. It also initializes a "config.json" file.*)
+ * directory if it does not exist. It also initializes a ".config" file. *)
 val init: string -> unit
 
 (* [load_config _] loads the configuration of server if there's already a 
  * configuration created for the server.
- * require: ".config" must exist in the current directory.
-*)
+ * requires: ".config" must exist in the current directory.
+ *)
 val load_config: unit -> config
 
-(* [verify token] is true if the token is correct.
- * requires: [config] is a caml_sync configuration 
+(* [verify token] returns true if the token is valid.
+ * requires: [config] is a valid caml_sync configuration
 *)
 val verify: config -> string -> bool
 
-(* [calc_diff_by_version from to] is the difference between version [from] and
+(* [calc_diff_by_version from to] returns the difference between version [from] and
  * version [to].
 *)
 val calc_diff_by_version: int -> int -> version_diff
@@ -37,23 +37,23 @@ val calc_diff_by_version: int -> int -> version_diff
 val handle_get_current_version: App.builder
 
 (* Handle POST request at "/diff". 
- * accepts: the diff json between the current client version and the latest
- * server version
- * effects: creates a new version in the server to store this new diff and update
- * [cur_version] to [cur_version + 1].
+ * accepts: the diff json representing the difference between the current client version 
+ * and the latest server version
+ * effects: updates [cur_version] to [cur_version + 1], and creates a new directory in the server 
+ * to store this new diff
  * returns: a json containing the [cur_version] to the client. *)
 val handle_post_diff_from_client: App.builder
 
 (* Handle GET request at "/diff/:client_version"
- * accepts: [client_version] is the current client version number.
- * returns: a diff json containing the different between the client version and
+ * accepts: the client's current version number ([client_version]).
+ * returns: a diff json containing the difference between the client version and
  * current server version. [cur_version] should also be included and sent back
  * to the client. *)
 val handle_get_diff_from_client: App.builder
 
 (* usage:
  *  caml_sync_server init <token> ->
- *    inits the current directory as a server directory
+ *    initializes the current directory as a server directory
  *  caml_sync_server ->
  *    runs the server
  *)

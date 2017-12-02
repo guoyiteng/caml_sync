f94d3a759f2b6866bc665ca2f18e7aee670bbc8a
Yizhou Yu <yy372@cornell.edu>
2017-12-02 10:37:36 -0500

    core

--- core.ml
+++ core.ml
@@ -1,1 +1,15 @@
-.
+type diff = Delete of string | Insert of string list
+
+let calc_diff base_content new_content = failwith "todo"
+
+let update_diff base_content diff_content = failwith "todo"
+
+let parse_json diff_json = failwith "todo"
+
+let build_json diff_obj = failwith "todo"
+
+let write_json w_json filename = failwith "todo"
+
+let create_file filename content = failwith "todo"
+
+let delete_file filename = failwith "todo"

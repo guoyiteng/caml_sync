b2e745b582f67dade27f8d9728438ee48810d139
Yizhou Yu <yy372@cornell.edu>
2017-12-02 11:13:45 -0500

    simpl compare algo

--- core.ml
+++ core.ml
@@ -1,10 +1,14 @@
-type command = Delete of string | Insert of string list
+type command = Delete of int | Insert of (int * string list)
 
 type diff = command list
 
 let calc_diff base_content new_content =
-  List.fold_right
-    (fun elem acc -> (Delete elem)::acc) base_content [Insert new_content]
+  let base_delete =
+    let rec add_delete from_index acc =
+      if from_index = 0 then acc
+      else add_delete (from_index - 1) ((Delete from_index)::acc)
+    in add_delete (List.length base_content) [] in
+  base_delete @ [Insert (0, new_content)]
 
 let update_diff base_content diff_content = failwith "todo"
 

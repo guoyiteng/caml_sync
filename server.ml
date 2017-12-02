5fefda18e84ddcc8b5eada2299dbfb054a014d93
Yiteng Guo <yg229@cornell.edu>
2017-12-02 14:22:29 -0500

    [add] finish server cli

--- server.ml
+++ server.ml
@@ -10,10 +10,10 @@ type config = {
   url: string;
   token: string;
   port: int;
-  version: int;
+  mutable version: int;
 }
 
-let config = ref {
+let config = {
     server_id = "default";
     url = "localhost";
     token = "password";
@@ -23,7 +23,13 @@ let config = ref {
 
 
 let init token =
-  raise Unimplemented
+  let open Ezjsonm in
+  dict ["server_id", (string config.server_id);
+        "url", (string config.url);
+        "token", (string token);
+        "port", (int config.port);
+        "version", (int config.version)] 
+        |> Ezjsonm.to_channel (open_out "config.json")
 
 let load_config () =
   raise Unimplemented
@@ -36,32 +42,40 @@ let calc_diff_by_version v_from v_to =
 
 let handle_get_current_version = get "/version/:token" begin fun req ->   
     let token = param req "token" in
-    if token = (!config.token) then
+    if token = config.token then
       `Json (
         let open Ezjsonm in
-        dict ["version", int (!config.version)]
+        dict ["version", int config.version]
       ) |> respond'
     else
+    (* Token is incorrect. *)
       `String ("Unauthorized Access") |> respond' ~code:`Unauthorized
-
   end
 
-let handle_post_diff_from_client = post "/diff" begin fun
+let handle_post_diff_from_client = post "/diff/:token" begin fun
     req ->
     raise Unimplemented 
   end
 
-let handle_get_diff_from_client = get "/diff" begin fun
+let handle_get_diff_from_client = get "/diff/:token" begin fun
     req ->
     raise Unimplemented
   end
 
 let main () =
-  App.empty
-  |> App.port 8080
-  |> handle_get_current_version
-  |> handle_post_diff_from_client
-  |> handle_get_diff_from_client
-  |> App.run_command
-
+  if Array.length Sys.argv = 1
+  then
+    App.empty
+    |> App.port 8080
+    |> handle_get_current_version
+    |> handle_post_diff_from_client
+    |> handle_get_diff_from_client
+    |> App.run_command
+  else if Array.length Sys.argv = 3 && Sys.argv.(1) = "init"
+  then
+    let token = Sys.argv.(2) in
+    init token
+  else
+    print_endline "Invalid arguments.
+    usage: ./caml_sync_server.native [init <token>]"
 let _ = main ()

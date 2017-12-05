c465ca98bd4142ad5672e2c578963bdc70783895
Yiteng Guo <yg229@cornell.edu>
2017-12-05 00:28:51 -0500

    [add] add debug.ml

---
+++ debug.ml
@@ -0,0 +1,11 @@
+open Core
+
+let old_1 = ["1"; "2"]
+let new_1 = ["3"; "4"]
+let _ = print_endline "dafasdf"
+let diff_1 = calc_diff old_1 new_1
+let diff_1 = calc_diff old_1 new_1
+let diff_1 = calc_diff old_1 new_1
+let diff_1 = calc_diff old_1 new_1
+let diff_1 = calc_diff old_1 new_1
+let diff_1 = calc_diff old_1 new_1

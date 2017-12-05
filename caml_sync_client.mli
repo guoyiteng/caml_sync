51da326fc58ecd970b7109e21908f5bb914a33b7
Yiteng Guo <yg229@cornell.edu>
2017-12-04 19:52:13 -0500

    [add] update client signature

--- caml_sync_client.mli
+++ caml_sync_client.mli
@@ -21,49 +21,54 @@ val get_latest_version : config -> int option
  * retrieves the current version of the working directory from [config],
  * sends a post request to the server to retrieve the difference between
  * the local version and the server's latest version, and returns the difference
- *)
+*)
 val get_update_diff : config -> version_diff option
 
 (* [post_local_diff config version_diff] sends the difference between the local version
  * and the server version to the server via json
- *)
+*)
 val post_local_diff : config -> version_diff -> int
 
 (* [compare_file filename] returns all the updates that the user has made
  * on the file represented by [filename] since the latest sync *)
- val compare_file : string -> file_diff
+val compare_file : string -> file_diff
 
- (* [check_invalid_filename ()] returns true if the local directory contains
-  * any file whose filename (excluding file extension) ends with "_local" *)
- val check_invalid_filename : unit -> bool
+(* [check_invalid_filename ()] returns true if the local directory contains
+ * any file whose filename (excluding file extension) ends with "_local" *)
+val check_invalid_filename : unit -> bool
 
 (* [compare_working_backup () ] returns a list of file_diff's that
  * have been modified after the last sync with the server.
  * The previous local version is stored in the hidden directory ".caml_sync/".
- *)
+*)
 val compare_working_backup : unit -> file_diff list
 
 (* [check_both_modified_files modified_file_diffs version_diff]
- * returns a list of filenames that indicates files that are inconsistent
- * in the following three versions: the local working version,
- * the remote server version, and the backup version in the hidden folder *)
-val check_both_modified_files : file_diff list -> version_diff -> string list
+ * returns a list of [(filename, is_deleted)] that indicates files that are 
+ * inconsistent in the following three versions: the local working version,
+ * the remote server version, and the backup version in the hidden folder. If 
+ * [is_deleted] is true, it means that that file is deleted in the local working 
+ * version compared with the backup version. *)
+val check_both_modified_files : 
+  file_diff list -> version_diff -> (string * bool) list
 
-(* [rename_both_modified both_modified] renames local files in [both_modified]
- * by appending "_local" to their filenames, because those files
- * have merge conflicts *)
-val rename_both_modified : string list -> unit
+(* [rename_both_modified both_modified_list] delete or renames local files in 
+ * [both_modified_list] by appending "_local" to their filenames,
+ * because those files have merge conflicts. [both_modified_list] is a list of
+ * [(filename, is_deleted)]. The [is_deleted] indicates whether we should delete
+ * or rename the file. If [is_deleted] is true, we should delete the file. *)
+val rename_both_modified : (string * bool) list -> unit
 
 (* [generate_client_version_diff server_diff]
  * returns: [None] if the current client has not made any update since the last sync,
  * otherwise returns [Some client_diff] where [client_diff] is the new update
  * that the current client has made
- *)
+*)
 val generate_client_version_diff : version_diff -> version_diff option
 
 (* [backup_working_files ignore_lst] copies all the files in current working
  * directory to ".caml_sync/", except those files in [ignore_lst]
- *)
+*)
 val backup_working_files : string list -> unit
 
 (* [init url token] creates a hidden ".config" file and stores [url] and [token]

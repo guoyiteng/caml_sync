7c43ca63b4f4131100aa491929f049e80910c573
hc626 <hc626@cornell.edu>
2017-12-07 02:34:25 -0500

    client test skeleton

--- caml_sync_client.ml
+++ caml_sync_client.ml
@@ -517,7 +517,7 @@ let main () =
         let tm = timestamp |> localtime in
         (List.nth week tm.tm_wday) ^ " " ^
         (List.nth month tm.tm_mon) ^ " " ^
-        (string_of_int tm.tm_mday) ^ " " ^ 
+        (string_of_int tm.tm_mday) ^ " " ^
         (string_of_int (tm.tm_year+1900)) ^ " " ^
         (string_of_int tm.tm_hour) ^ ":" ^ (string_of_int tm.tm_min) in
       if Array.length Sys.argv = 3 && (Array.get Sys.argv 2) = "list" then
@@ -561,13 +561,3 @@ let main () =
         print_endline "All local conflict files have been removed." end
       else raise (Invalid_argument ("Invalid arguments.\n" ^ usage))
     | _ -> raise (Invalid_argument ("Invalid arguments.\n" ^ usage))
-
-(* let () =
-  try main () with
-  | Unauthorized -> print_endline ("Your token is wrong.\n" ^ "Please run 'camlsync clean' and 'camlsync init <url> <token>' to reinitialize your server.")
-  | Timeout -> print_endline "Your request is time out."
-  | Unix.Unix_error _ -> print_endline ("Cannot connect to the server." ^ "\nPlease double check your server address and make sure your server is running.")
-  | ServerError e -> print_endline e
-  | Bad_request s -> print_endline s
-  | Not_Initialized -> print_endline "Current directory has not been initialized."
-  | Invalid_argument s -> print_endline s *)
--- client_test.ml
+++ client_test.ml
@@ -1,14 +1,13 @@
 open OUnit2
 open Caml_sync_client
 
+
 let config = {
   client_id="1";
   url="127.0.0.1:8080";
   token="sb";
   version=0
 }
-
-let () = update_config config
 
 let config_tests = [
   "basic_write_read_consistency" >::(fun _ -> assert_equal
@@ -18,9 +17,31 @@ let config_tests = [
                         {config with version=1});
 ]
 
+let helpers_tests = [
+  "test_compare_file" >:: (fun _ -> ());
+  "test_check_invalid_filename" >:: (fun _ -> ());
+  "test_compare_working_backup" >:: (fun _ -> ());
+  "test_check_both_modified_files" >:: (fun _ -> ());
+  "rename_both_modified" >:: (fun _ -> ());
+  "generate_client_version_diff" >:: (fun _ -> ());
+  "test_backup_working_files" >:: (fun _ -> ())
+]
+
+let request_tests = [
+  "test_init" >:: (fun _ -> ());
+  "test_sync" >:: (fun _ -> ());
+  "test_get_update_diff" >:: (fun _ -> ());
+  "test_post_local_diff" >:: (fun _ -> ());
+  "test_history_list" >:: (fun _ -> ());
+  "test_time_travel" >:: (fun _ -> ());
+
+]
+
 let tests =
   "test suite for client"  >::: List.flatten [
     config_tests;
+    helpers_tests;
+    request_tests
   ]
 
 let _ = run_test_tt_main tests
